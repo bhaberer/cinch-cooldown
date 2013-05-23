1877b54011ad7a7f0d3d26858e916f32cb542324
Brian Haberer <bhaberer@gmail.com>
2013-05-23 16:48:40 -0700

    more less complexness!

--- lib/cinch-cooldown.rb
+++ lib/cinch-cooldown.rb
@@ -17,52 +17,59 @@ module Cinch
       # return if we don't have a cooldown config
       return true unless shared[:cooldown] && shared[:cooldown][:config]
       synchronize(:cooldown) do
+        # return if we don't have a channel (i.e. user is pming the bot)
+        return false if m.channel.nil?
+
+        channel = m.channel.name
+        user    = m.user.nick
+
         # Make sure the configuration is sane.
-        return true unless configuration_check?(m)
+        return true if configuration_broken?(channel)
 
-        # Normal usage stuff starts here, check and see if the channel time is up
-        if channel_cooldown_finished?(channel)
-          # channel cd is up, check per user by checking if the user's even triggered a cd yet
-          if shared[:cooldown][channel].key?(user)
-            # User's in the config, check time
-            if user_cooldown_finished?(channel, user)
-              # Their time's up, run the command
-              trigger_cooldown_for(channel, user)
-              return true
-            end
-          else
-            # User's not used bot before, run the command
-            trigger_cooldown_for(channel, user)
-            return true
-          end
-        end
+        # Make sure it's not the first command
+        return true if first_run?(channel, user)
 
-        # Handle cooldowns here
+        # Check if timers are finished
+        return true if cooldowns_finished?(channel, user)
+
+        # Handle unfinished cooldowns here
         m.user.notice cooldown_message(channel, user)
         return false
       end
     end
 
-    def configuration_check?(m)
-      # return if we don't have a channel (i.e. user is pming the bot)
-      return false if m.channel.nil?
-
-      channel = m.channel.name
-      user    = m.user.nick
-
+    def configuration_broken?(channel)
       # return if the config doesn't smell right for this channel
-      return false unless shared[:cooldown][:config].key?(channel) &&
-                          shared[:cooldown][:config][channel].key?(:global)
-                          shared[:cooldown][:config][channel].key?(:user)
+      return true unless shared[:cooldown][:config].key?(channel) &&
+                         shared[:cooldown][:config][channel].key?(:global) &&
+                         shared[:cooldown][:config][channel].key?(:user)
 
-      # Check and see if the cooldown has been triggered for this run yet
+    end
+
+    def first_run?(channel)
       unless shared[:cooldown].key?(channel)
         trigger_cooldown_for(channel, user)
-        return false
+        return true
       end
+    end
 
-      # Assuming we didn't hit anything strange, return
-      return true
+    def cooldowns_finished?(channel, user)
+      # Normal usage stuff starts here, check and see if the channel time is up
+      if channel_cooldown_finished?(channel)
+        # channel cd is up, check per user by checking if the user's even triggered a cd yet
+        if shared[:cooldown][channel].key?(user)
+          # User's in the config, check time
+          if user_cooldown_finished?(channel, user)
+            # Their time's up, run the command
+            trigger_cooldown_for(channel, user)
+            return true
+          end
+        else
+          # User's not used bot before, run the command
+          trigger_cooldown_for(channel, user)
+          return true
+        end
+      end
     end
 
     def cooldown_message(channel, user)
